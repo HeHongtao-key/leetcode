a38884d31e8e91fe5455d52fc8640db85e08dcb3
hehongtao <hehongtao@lianjia.com>
2019-03-11 12:32:06 +0800

    104. 二叉树的最大深度

--- MaximumDepthofBinaryTree/Solution.java
+++ MaximumDepthofBinaryTree/Solution.java
@@ -2,9 +2,13 @@ package MaximumDepthofBinaryTree;
 
 public class Solution {
     public int maxDepth(TreeNode root) {
-        if(root == null){
+        if (root == null) {
             return 0;
         }
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public int maxDepth2(TreeNode root) {
+        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
