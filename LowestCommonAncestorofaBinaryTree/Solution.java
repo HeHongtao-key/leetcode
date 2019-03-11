b911e7dacb01d1549fa07b36616d237389e362fe
hehongtao <hehongtao@lianjia.com>
2019-03-11 12:18:16 +0800

    700. 二叉搜索树中的搜索

---
+++ LowestCommonAncestorofaBinaryTree/Solution.java
@@ -0,0 +1,4 @@
+package LowestCommonAncestorofaBinaryTree;
+
+public class Solution {
+}
---
+++ SearchinaBinarySearchTree/TreeNode.java
@@ -0,0 +1,8 @@
+package SearchinaBinarySearchTree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
